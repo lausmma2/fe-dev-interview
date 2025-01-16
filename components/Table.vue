4d29a33cc4d8baa14c6df87145b8eabaf5f37e5f
Marek Laušman <marek.lausman@bootiq.io>
2025-01-16 18:49:12 +0100

    feat: add table component

---
+++ components/Table.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  type Row = { [key: string]: string };
+  type Column = string;
+
+  const props = defineProps<{
+    rows: Row[];
+    columns: Column[];
+  }>();
+</script>
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left">
+      <!-- Table Header -->
+      <thead class="text-md text-primary capitalize bg-gray-300">
+        <tr>
+          <th v-for="column in props.columns" :key="column" scope="col" class="px-6 py-3">
+            {{ column }}
+          </th>
+        </tr>
+      </thead>
+      <!-- Table Body -->
+      <tbody>
+        <tr
+          v-for="(row, index) in props.rows"
+          :key="index"
+          class="bg-white border-b hover:bg-blue-50"
+        >
+          <th scope="row" class="px-6 py-4 font-semibold text-primary text-md whitespace-nowrap">
+            {{ row[props.columns[0]] }}
+          </th>
+          <td v-for="column in props.columns.slice(1)" :key="column" class="px-6 py-4">
+            {{ row[column] }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
   import { setPageTitle } from '~/utils/pageTitle';
+  import Table from '~/components/Table.vue';
   setPageTitle('Users');
+
+  const columns = ['email', 'name', 'status'];
+  const _rowsMocked = [
+    { email: 'test@test.cz', name: 'Test 1', status: 'active' },
+    { email: 'test@test.eu', name: 'Test 2', status: 'inactive' },
+    { email: 'test@test.com', name: 'Test 3', status: 'active' },
+  ];
 </script>
 
 <template>
   <div class="h-screen flex justify-center items-center">
-    <div class="inline-block bg-white p-10 rounded-lg text-center">User Page</div>
+    <Table :columns="columns" :rows="_rowsMocked" />
   </div>
 </template>
